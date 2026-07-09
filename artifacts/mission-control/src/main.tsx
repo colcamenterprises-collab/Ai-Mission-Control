0046bad7834ef2d79f8cefe722b2d87753f8188d
colcamenterprises-collab <colcamenterprises@gmail.com>
2026-07-09 21:40:14 +0700

    Load Mission Control visual styles

--- artifacts/mission-control/src/main.tsx
+++ artifacts/mission-control/src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import { setAuthTokenGetter } from "@workspace/api-client-react";
 import "./index.css";
+import "./mission-ui.css";
 
 setAuthTokenGetter(() => localStorage.getItem("mission_control_admin_token"));
 
